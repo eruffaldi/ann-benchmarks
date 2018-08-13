8cdbb65fd7dea2bdc977100eb9f487f2df37133c
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2018-08-13 10:06:52 +0200

    release and native for positkdd and flann

--- install/deploy_kdtree.sh
+++ install/deploy_kdtree.sh
@@ -5,5 +5,7 @@ git clone https://$1:$2@bitbucket.org/eruffaldi/posit_kdtree.git
 cd posit_kdtree
 mkdir build
 cd build
-cmake -DPython_ADDITIONAL_VERSIONS=3.7 -DPYTHON_LIBRARIES="/usr/lib/libpython37.dylib" -DPYTHON_INCLUDE_DIRS="/usr/include/python/3.7" -DPYTHON_EXECUTABLE="/usr/bin/python3" -DPYTHON_VERSION_SUFFIX=3 -DCMAKE_POSITION_INDEPENDENT_CODE=ON ..
-make
+export CXXFLAGS=-march=native
+cmake -DCMAKE_BUILD_TYPE=Release -DPython_ADDITIONAL_VERSIONS=3.7 -DPYTHON_LIBRARIES="/usr/lib/libpython37.dylib" -DPYTHON_INCLUDE_DIRS="/usr/include/python/3.7" -DPYTHON_EXECUTABLE="/usr/bin/python3" -DPYTHON_VERSION_SUFFIX=3 -DCMAKE_POSITION_INDEPENDENT_CODE=ON ..
+make -j
+make install
--- install/deploy_softfloat.sh
+++ install/deploy_softfloat.sh
@@ -5,5 +5,7 @@ git clone --depth 1 https://github.com/eruffaldi/berkeley-softfloat-3.git
 cd berkeley-softfloat-3
 mkdir build
 cd build
-cmake -DCMAKE_POSITION_INDEPENDENT_CODE=ON ..
+export CXXFLAGS=-march=native
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON ..
+make -j
 make install
